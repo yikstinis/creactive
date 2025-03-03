f36f3794fa557458a13d9a1e36503aae049c573a
yikstinis <yikstinis@gmail.com>
2025-03-03 18:37:25 +0100

    Added react dependency TODO warning.

--- jest-setup.web.ts
+++ jest-setup.web.ts
@@ -1,2 +1,9 @@
+// TODO: Keep an eye on react-server-dom-webpack dependency!
+// It do not supports our react version, wants react@19 at least (it is listed).
+// It is a dependency of jest-expo and is used for renderToReadableStream call.
+// Seems like react@19 is rendering for our web tests.
+// We can't update to react@19 yet, because of the react-native-web dependency.
+// It has no support for react@19 yet, but later we can update I guess!
+
 // This file is used to setup tests that run in a native environment.
 import '@testing-library/jest-dom'
