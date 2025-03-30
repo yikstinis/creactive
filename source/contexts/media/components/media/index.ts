26815ba2c5e83d82faad72f9658aec837335a49a
yikstinis <yikstinis@gmail.com>
2025-03-30 15:49:02 +0200

    Components default re-export error fixed.

--- source/contexts/media/components/media/index.ts
+++ source/contexts/media/components/media/index.ts
@@ -2,7 +2,7 @@ import { Platform } from 'react-native'
 import type { MediaComponent } from './media.types'
 
 export const Media: MediaComponent = Platform.select({
-  web: require('./media.web'),
-  default: require('./media.native'),
+  web: require('./media.web').default,
+  default: require('./media.native').default,
 })
 export type { MediaComponent, MediaProps } from './media.types'
--- source/contexts/media/components/wrapper/index.ts
+++ source/contexts/media/components/wrapper/index.ts
@@ -2,6 +2,6 @@ import { Platform } from 'react-native'
 import type { WrapperComponent } from './wrapper.types'
 
 export const Wrapper: WrapperComponent = Platform.select({
-  web: require('./wrapper.web'),
-  default: require('./wrapper.native'),
+  web: require('./wrapper.web').default,
+  default: require('./wrapper.native').default,
 })
