bfc242cafa39c9a78450e4ebac3220d12b56b289
Mark Sinitski <yikstinis@gmail.com>
2026-06-16 19:18:02 +0200

    Add DIMENSION_ZERO constant and bump version. (#28)

--- source/helpers/dimension/index.ts
+++ source/helpers/dimension/index.ts
@@ -1,12 +1,13 @@
 import { Dimension } from './dimension'
 
+export type { PercentDimension, PixelDimension } from './dimension'
 export type {
   DimensionValue,
   PercentDimensionValue,
   PixelDimensionValue,
 } from './dimension.types'
 export { Dimension }
-export type { PercentDimension, PixelDimension } from './dimension'
 
+export const DIMENSION_ZERO = new Dimension(0)
 export const DIMENSION_MIN = new Dimension(0, Dimension.Unit.PERCENT)
 export const DIMENSION_MAX = new Dimension(100, Dimension.Unit.PERCENT)
