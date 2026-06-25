d9944beaecc76245ebf5862aa01684a590132103
Marek Sinitski <marek.sinitski@metapic.com>
2026-06-25 23:51:58 +0200

    Improved icon context test with console.error suppression and faker color.

--- source/components/atoms/icon/icon.context.test.tsx
+++ source/components/atoms/icon/icon.context.test.tsx
@@ -1,3 +1,5 @@
+import type { Color } from '@/types/color.types'
+import { faker } from '@faker-js/faker'
 import { renderHook } from '@testing-library/react-native'
 import type { PropsWithChildren } from 'react'
 import { IconContext, useIconContext } from './icon.context'
@@ -6,13 +8,16 @@ describe('@/components/atoms/icon', () => {
   describe('icon.context', () => {
     describe('useIconContext', () => {
       it('throws when used outside provider', () => {
+        jest.spyOn(console, 'error').mockImplementation(() => {})
         expect(() => renderHook(() => useIconContext())).toThrow(
           "Can't use icon context outside of its provider!",
         )
+        jest.restoreAllMocks()
       })
 
       it('returns context value when inside provider', () => {
-        const colorValue = 'rgb(25,25,30)' as const
+        const colorValue =
+          `rgb(${faker.number.int({ max: 255 })},${faker.number.int({ max: 255 })},${faker.number.int({ max: 255 })})` satisfies Color
         const wrapper = ({ children }: PropsWithChildren) => (
           <IconContext.Provider value={{ colorValue }}>
             {children}
